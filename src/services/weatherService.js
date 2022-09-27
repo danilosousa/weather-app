2cb7858b699d94243304e2f163fd25fd72b4c200
danilosousa <danilosousa.developer@outlook.com>
2022-09-26 21:32:38 -0300

    hide console

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import config from "../config/config";
 
 async function getWeather(lon, lat) {
-  console.log(lon, lat);
+  //console.log(lon, lat);
   const response = await axios.get(
     `${config.WEATHER_DATA_ENDPOINT}lon=${lon}&lat=${lat}`
   );
